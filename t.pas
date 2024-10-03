22553b33c7739fd37bec6f772ca64acd95d082f0
Андрей Тимофеев <timo@sfedu.ru>
2024-10-03 09:42:34 +0300

    Третье задание ОП

---
+++ t.pas
@@ -0,0 +1,6 @@
+﻿begin
+  var hours := readinteger('Введите количество часов:');
+  var minutes := readinteger('Введите количество минут:');
+  var seconds := readinteger('Введите количество секунд:');
+  print(3600*hours + 60*minutes + seconds);
+end.
--- time.pas
+++ time.pas
@@ -1,6 +1,6 @@
 ﻿begin
   var year := readinteger('Введите номер года:');
   if (year mod 400 = 0) or (year mod 4 = 0) and (year mod 25 <> 0) then
-    print(True)
-  else print(False)
+    print(365)
+  else print(366)
 end.
