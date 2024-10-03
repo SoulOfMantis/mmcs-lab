4885f8740c053afeeb612c19d8013faa408d52c6
SoulOfMantis <timo@sfedu.ru>
2024-10-03 12:43:50 +0300

    Удалён дубль

--- t.pas
+++
@@ -1,6 +0,0 @@
-﻿begin
-  var hours := readinteger('Введите количество часов:');
-  var minutes := readinteger('Введите количество минут:');
-  var seconds := readinteger('Введите количество секунд:');
-  print(3600*hours + 60*minutes + seconds);
-end.
